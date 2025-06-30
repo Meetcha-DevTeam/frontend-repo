27bf3575b248c79ab27d10630daf7a87a8eff359
Chris Ahn <chrisahn1007@gmail.com>
2025-07-01 00:26:52 +0900

    feat : Header 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MonthlySchedulePage from "./pages/schedule/monthly_schedule/MonthlySchedulePage";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index path="/" element={<MonthlySchedulePage />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/pages/schedule/monthly_schedule/MonthlySchedulePage.tsx
@@ -0,0 +1,12 @@
+import styles from "./MonthlySchedulePage.module.scss";
+import Header from "@/components/Header";
+
+const MonthlySchedulePage = () => {
+  return (
+    <div className={styles.monthly_schedule_page}>
+      <Header />
+    </div>
+  );
+};
+
+export default MonthlySchedulePage;
