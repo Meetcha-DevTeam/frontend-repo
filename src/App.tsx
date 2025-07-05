2db6b85d4bfd1596837b6d7c76ac83cd8387e86d
Chris Ahn <chrisahn1007@gmail.com>
2025-07-05 22:30:13 +0900

    refactor : 홈 섹션을 고려한 전체 컴포넌트 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SchedulePage from "./pages/schedule/SchedulePage";
+import BackgroundPage from "./pages/background/BackgroundPage";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index path="/" element={<SchedulePage />}></Route>
+        <Route index path="/" element={<BackgroundPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
