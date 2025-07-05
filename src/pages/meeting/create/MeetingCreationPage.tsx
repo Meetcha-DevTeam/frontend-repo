0e1707243ef12a7144d961243f7d4c040c486857
Chris Ahn <chrisahn1007@gmail.com>
2025-07-06 01:26:36 +0900

    feat : TopNav 컴포넌트 구현

--- src/pages/meeting/create/MeetingCreationPage.tsx
+++ src/pages/meeting/create/MeetingCreationPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styles from "./MeetingCreationPage.module.scss";
 import Button from "@/components/Button";
+import TopNav from "@/components/TopNav";
 
 const MeetingCreationPage = () => {
   return (
     <div className={styles.meetingCreationPage}>
+      <TopNav type="text" label="미팅 생성" />
       MeetingCreationPage
       <Button label={"미팅 생성하기"} />
     </div>
