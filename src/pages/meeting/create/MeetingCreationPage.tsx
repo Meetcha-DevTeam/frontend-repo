bdd9f3b080b5bdec4fea7aa8ff38ef715f580533
Chris Ahn <chrisahn1007@gmail.com>
2025-07-06 17:38:48 +0900

    feat : MeetingCreationView 컴포넌트 초기 구조 구현

--- src/pages/meeting/create/MeetingCreationPage.tsx
+++ src/pages/meeting/create/MeetingCreationPage.tsx
@@ -1,14 +1,16 @@
-import React from "react";
 import styles from "./MeetingCreationPage.module.scss";
 import Button from "@/components/Button";
 import TopNav from "@/components/TopNav";
+import MeetingCreationView from "./MeetingCreationView";
 
 const MeetingCreationPage = () => {
   return (
     <div className={styles.meetingCreationPage}>
       <TopNav type="text" label="미팅 생성" />
-      MeetingCreationPage
-      <Button label={"미팅 생성하기"} />
+      <div className={styles.meetingCreationPage__contents}>
+        <MeetingCreationView />
+        <Button label={"미팅 생성하기"} />
+      </div>
     </div>
   );
 };
