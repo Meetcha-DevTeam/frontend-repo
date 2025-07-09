065be3e7d9bb94bb93c086268244ad17c8249f2e
Chris Ahn <chrisahn1007@gmail.com>
2025-07-10 01:35:24 +0900

    feat : CalendarInputComponent 로직 및 UI 구현

---
+++ src/pages/meeting/create/input_component/CalendarInputComponent.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import styles from "./CalendarInputComponent.module.scss";
+import Calendar from "react-calendar";
+import "./Calendar.scss";
+import { dateFormatter } from "@/utils/dateFormatter";
+
+const CalendarInputComponent = () => {
+  const [clickedDays, setClickedDays] = useState<string[]>([]);
+
+  const clickDaySaver = (value: Date) => {
+    const clickedDay = dateFormatter(value);
+    console.log(clickedDay);
+    if (clickedDays.includes(clickedDay)) {
+      setClickedDays((prevClickedDays) => prevClickedDays.filter((day) => day !== clickedDay));
+      return;
+    }
+    setClickedDays((prevClickedDays) => [...prevClickedDays, clickedDay]);
+  };
+
+  useEffect(() => {
+    console.log(clickedDays);
+  }, [clickedDays]);
+
+  return (
+    <div className={styles.calendarInputComponent}>
+      <Calendar
+        formatDay={(locale, date) => date.toLocaleString("en", { day: "numeric" })}
+        onClickDay={clickDaySaver}
+        tileClassName={({ date, view }) => {
+          if (clickedDays.includes(dateFormatter(date))) {
+            return "custom-active";
+          }
+        }}
+      />
+    </div>
+  );
+};
+
+export default CalendarInputComponent;
