547ffe76faa037675af90b707f56f1c414c103f6
KIM-korea <dohyun3885@gmail.com>
2025-07-15 20:21:40 +0900

    chore:memoir_write수정

--- src/pages/memoir/Memoir_write/Memoir_write_intro.tsx
+++ src/pages/memoir/Memoir_write/Memoir_write_intro.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import leftchevron from "@/assets/LeftChevron.svg";
+import { useNavigate } from "react-router-dom";
 
 import "./Memoir_write.scss";
 
 const Memoir_write_intro = () => {
+  const navigate=useNavigate();
+
+  const handleClick=()=>{
+    navigate("/memoir");
+  };
   return (
     <div className="memoir_write_intro_container">
-      <button>
+      <button onClick={handleClick}>
         <img src={leftchevron} alt="leftchevron"></img>
       </button>
       <p>회고 작성하기</p>
