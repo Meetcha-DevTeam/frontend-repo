9bc1ab26d435aa81c410ca43fa1f63b8f8f2ed7e
Chris Ahn <chrisahn1007@gmail.com>
2025-07-29 01:01:36 +0900

    feat : Mock Service Worker 설치 및 초기세팅

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,27 @@
+import { http } from "msw";
+
+export const handlers = [
+  http.get("/user/schedule", () => {
+    return Response.json({
+      isSuccess: true,
+      code: 200,
+      message: "일정 조회 성공",
+      data: [
+        {
+          eventId: "abc123xyz",
+          title: "회의",
+          startAt: "2025-08-01T14:00:00",
+          endAt: "2025-08-01T15:00:00",
+          recurrence: "매주", // or "매일", "매월", "NONE"
+        },
+        {
+          eventId: "def456uvw",
+          title: "점심 시간",
+          startAt: "2025-08-02T12:00:00",
+          endAt: "2025-08-02T13:00:00",
+          recurrence: "매주", // or "매일", "매월", "NONE"
+        },
+      ],
+    });
+  }),
+];
