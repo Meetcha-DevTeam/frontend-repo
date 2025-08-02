b2a6544b423524fe57b7954e6d2f8bccb9cd546f
Chris Ahn <chrisahn1007@gmail.com>
2025-08-02 18:53:58 +0900

    fix : TimePicker 컴포넌트 스크롤 로직 및 UI 수정

--- src/mocks/browser.ts
+++ src/mocks/browser.ts
@@ -2,6 +2,3 @@ import { setupWorker } from "msw/browser";
 import { handlers } from "./handlers";
 
 export const worker = setupWorker(...handlers);
-worker.events.on("request:unhandled", ({ request }) =>
-  console.warn("🛑  unhandled →", request.method, request.url)
-);
