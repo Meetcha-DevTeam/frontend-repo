cdd6997c38f994a8070505bdf710a9d613975c8b
Chris Ahn <chrisahn1007@gmail.com>
2025-08-19 00:59:32 +0900

    fix : 일정 생성 api 에 동봉하는 데이터 포맷 수정

--- src/apis/meeting/meetingAPI.ts
+++ src/apis/meeting/meetingAPI.ts
@@ -46,10 +46,6 @@ export const createMeeting = async (data) => {
   switch (res.code) {
     case 201:
       alert(res.message);
-
-      navigate("/participate",{state:res.data.meetingId});
-      break;
-
       return res.code;
 
     case 400:
