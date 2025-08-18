f7f71e6fb6e6bd70c3bbad23e165c4491445d360
Chris Ahn <chrisahn1007@gmail.com>
2025-08-18 20:30:12 +0900

    feat : 401 응답일시 access token을 갱신하고 원요청을 다시 수행할 수 있도록 apiCall과 authAPI 수정

--- src/apis/auth/authAPI.ts
+++ src/apis/auth/authAPI.ts
@@ -10,3 +10,17 @@ export const sendAuthCode = async (authCode: string) => {
   console.log(res);
   return res;
 };
+
+export const renewAccessToken = async (refToken: string) => {
+  const data = { refreshToken: refToken };
+  const res: ApiResponse<AuthResponse> = await apiCall("oauth/refresh", "POST", data, false);
+  if (res.code === 200) {
+    console.log(res.message);
+    sessionStorage.setItem("access-token", res.data.accessToken);
+    sessionStorage.setItem("refresh-token", res.data.refreshToken);
+    return true;
+  } else {
+    console.error(res.message);
+    return false;
+  }
+};
