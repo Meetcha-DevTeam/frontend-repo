42fb25ccafdce466ea67c2144405f86e9dea9d0b
Younsang Na <77400131+nayounsang@users.noreply.github.com>
2025-08-20 13:36:27 +0000

    feat: 미팅 참여 완료 성공시 상세 페이지러 이동

--- src/pages/participate/Participate_timetable/Participate_timetable_ctn.tsx
+++ src/pages/participate/Participate_timetable/Participate_timetable_ctn.tsx
@@ -142,8 +142,7 @@ const Participate_timetable_ctn = () => {
         // 참여 성공 후 이동/알림 처리
         alert("미팅 참여 성공!");
         console.log(res);
-        navigate(`/schedule`);
-        // navigate(`/meeting/${meetingId}`);
+        navigate(`/meeting/${meetingId}`);
       } else if (res.code === 409) {
         alert("이미 이 미팅에 참가했습니다.");
       } else if (res.code === 400) {
