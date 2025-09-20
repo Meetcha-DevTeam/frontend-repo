1fef72c7a92f04c099bde00bfcc3527222f9edef
nayounsang <nayounsang722@gmail.com>
2025-09-21 02:30:14 +0900

    refactor: ui logic을 폼 데이터 관리 로직과 분리

--- src/pages/meeting/create/MeetingCreationView.tsx
+++ src/pages/meeting/create/MeetingCreationView.tsx
@@ -1,45 +1,69 @@
-import React, { useEffect, useState } from "react";
+import React, { useRef } from "react";
 import styles from "./MeetingCreationView.module.scss";
 import MeetingOptionCard from "./MeetingOptionCard";
 import type { MeetingSendData } from "./MeetingCreationPage";
 import { cardDataSet } from "./constants/MeetingCreation.constants";
-import { deadlineParse, durationMinutesParse, projectDataParse } from "@/utils/MeetingCreationUtils";
+import {
+  useMeetingCreateFormContext,
+} from "./hooks/useMeetingCreateForm";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionHeader,
+  AccordionItem,
+  AccordionTrigger,
+} from "@radix-ui/react-accordion";
+import Pencil from "@assets/pencil.svg?react";
+import DownArrow from "@assets/downArrow.svg?react";
+import Calendar from "@assets/calendar.svg?react";
+import TextInputComponent from "./input_component/TextInputComponent";
+import type { MeetingCreationSchema } from "./schemas/meetingCreationSchema";
 
 interface Props {
   setAllDataReserved: React.Dispatch<React.SetStateAction<boolean>>;
   setCompleteData: React.Dispatch<React.SetStateAction<MeetingSendData>>;
 }
 
-const MeetingCreationView = ({ setAllDataReserved, setCompleteData }: Props) => {
-  const [meetingTitle, setMeetingTitle] = useState<string>("");
-  const [meetingDescription, setMeetingDescription] = useState<string>("");
-  const [meetingCandidateDates, setMeetingCandidateDates] = useState<string[]>([]);
-  const [durationMinutes, setDurationMinutes] = useState<string>("");
-  const [deadline, setDeadline] = useState<string>("");
-  const [projectData, setProjectData] = useState<string>(null);
-  const [clickedCardNum, setClickedCardNum] = useState<number>(null); // 미팅 카드 중 유저가 클릭한 카드의 번호
+const fieldNames: Record<
+  keyof MeetingCreationSchema,
+  keyof MeetingCreationSchema
+> = {
+  title: "title",
+  description: "description",
+  candidateDates: "candidateDates",
+  durationMinutes: "durationMinutes",
+  deadline: "deadline",
+  projectId: "projectId",
+};
 
-  const stateMapping = {
-    description: { data: meetingDescription, dataSetter: setMeetingDescription },
-    candidateDates: { data: meetingCandidateDates, dataSetter: setMeetingCandidateDates },
-    durationMinutes: { data: durationMinutes, dataSetter: setDurationMinutes },
-    deadline: { data: deadline, dataSetter: setDeadline },
-    project: { data: projectData, dataSetter: setProjectData },
-  };
+const TriggerContent = ({
+  icon,
+  title,
+  value,
+}: {
+  icon: React.ReactNode;
+  title: string;
+  value: string | string[];
+}) => {
+  return (
+    <>
+      {icon}
+      <div className={styles.accordion__trigger__content}>
+        <p className={styles.accordion__trigger__content__title}>{title}</p>
+        <p className={styles.accordion__trigger__content__value}>
+          {typeof value === "string" ? value : value.join(", ")}
+        </p>
+      </div>
+      <DownArrow className={styles.accordion__trigger__chevron} />
+    </>
+  );
+};
 
-  useEffect(() => {
-    if (meetingTitle && meetingCandidateDates.length > 0 && durationMinutes && deadlineParse(deadline)) {
-      setAllDataReserved(true);
-      setCompleteData({
-        title: meetingTitle,
-        description: meetingDescription,
-        candidateDates: meetingCandidateDates,
-        durationMinutes: durationMinutesParse(durationMinutes),
-        deadline: deadlineParse(deadline),
-        projectId: projectDataParse(projectData),
-      });
-    }
-  }, [meetingTitle, meetingDescription, meetingCandidateDates, durationMinutes, deadline, projectData]);
+const MeetingCreationView = ({
+  setAllDataReserved,
+  setCompleteData,
+}: Props) => {
+  const form = useMeetingCreateFormContext();
 
   return (
     <div className={styles.meetingCreationView}>
@@ -48,24 +72,59 @@ const MeetingCreationView = ({ setAllDataReserved, setCompleteData }: Props) => 
         type="text"
         placeholder="미팅 제목을 적어주세요"
         onChange={(e) => {
-          setMeetingTitle(e.target.value);
+          form.setFormValue("title", e.target.value);
         }}
       />
-      <div className={styles.meetingCreationView__meetingOptionContainer}>
-        {cardDataSet.map((item, _) => (
-          <MeetingOptionCard
-            key={item.id}
-            title={item.title}
-            icon={item.icon}
-            data={stateMapping[item.stateKey as keyof typeof stateMapping].data}
-            dataSetter={stateMapping[item.stateKey as keyof typeof stateMapping].dataSetter}
-            type={item.id}
-            clickedCardNum={clickedCardNum}
-            setCardClickedNum={setClickedCardNum}
-            meetingCandidateDates={meetingCandidateDates}
-          />
-        ))}
-      </div>
+      <Accordion
+        type="single"
+        collapsible
+        className={styles.accordion}
+      >
+        <AccordionItem
+          value={fieldNames.description}
+          className={styles.accordion__item}
+        >
+          <AccordionHeader>
+            <AccordionTrigger className={styles.accordion__trigger}>
+              <TriggerContent
+                icon={<Pencil className={styles.accordion__trigger__icon} />}
+                title="미팅 설명"
+                value={form.getFormValue("description") || "-"}
+              />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent className={styles.accordion__content}>
+            <textarea
+              style={{
+                width: "100%",
+                height: "4rem",
+                padding: "5px",
+                border: "none",
+                outline: "none",
+                fontFamily: "Pretendard",
+                resize: "none",
+              }}
+              value={form.getFormValue("description") || ""}
+              onChange={(e) => {
+                form.setFormValue("description", e.target.value);
+              }}
+            />
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem
+          value={fieldNames.candidateDates}
+          className={styles.accordion__item}
+        >
+          <AccordionHeader>
+            <AccordionTrigger className={styles.accordion__trigger}>
+              <TriggerContent icon={<Calendar className={styles.accordion__trigger__icon} />} title="미팅 후보 날짜" value={form.getFormValue("candidateDates")} />
+            </AccordionTrigger>
+          </AccordionHeader>
+          <AccordionContent className={styles.accordion__content}>
+            미구현
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
