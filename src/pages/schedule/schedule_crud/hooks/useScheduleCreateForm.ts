17d08087cce4bff21694a30824968202590e23e1
Chris Ahn <chrisahn1007@gmail.com>
2025-10-27 13:51:09 +0900

    fix(src/schedule_crud): formDataSetter에 useCallback 훅 사용
    - 이전 커밋에서 ScheduleCrudCardExpandable의 useEffect에서
      setFormValue로 의존성 배열을 변경하기는 했지만, setFormValue로 값을
      업데이트할 때마다 ScheduleCreateFormContext.Provider도 리렌더링되고,
      그러면 setFormValue도 새로운 함수로 재생성되면서 다시 useEffect가
      실행되는 무한루프가 발생
    - 따라서 setFormValue에 해당하는 formDataSetter메서드를 useCallback으로
      감싸주었다

--- src/pages/schedule/schedule_crud/hooks/useScheduleCreateForm.ts
+++ src/pages/schedule/schedule_crud/hooks/useScheduleCreateForm.ts
@@ -3,7 +3,7 @@ import {
   scheduleCreationSchema,
   type ScheduleCreationSchema,
 } from "../schemas/scheduleCreationSchema";
-import { createContext, useContext, useState } from "react";
+import { createContext, useCallback, useContext, useState } from "react";
 
 export const useScheduleCreateForm = (
   setter?: FormSetter<ScheduleCreationSchema>
@@ -21,15 +21,18 @@ export const useScheduleCreateForm = (
     Record<keyof ScheduleCreationSchema, string>
   > | null>(null);
 
-  const formDataSetter = (
-    key: keyof ScheduleCreationSchema,
-    value: ScheduleCreationSchema[keyof ScheduleCreationSchema]
-  ) => {
-    setFormData((prev) => ({
-      ...prev,
-      [key]: value,
-    }));
-  };
+  const formDataSetter = useCallback(
+    (
+      key: keyof ScheduleCreationSchema,
+      value: ScheduleCreationSchema[keyof ScheduleCreationSchema]
+    ) => {
+      setFormData((prev) => ({
+        ...prev,
+        [key]: value,
+      }));
+    },
+    []
+  );
 
   const getFormValue = <K extends keyof ScheduleCreationSchema>(key: K) => {
     return formData[key];
