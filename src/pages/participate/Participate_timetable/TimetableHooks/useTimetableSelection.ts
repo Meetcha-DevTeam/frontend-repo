fcc5ec91394fc7aeb0e594bdb89d7f89ebb801af
KIM-korea <dohyun3885@gmail.com>
2025-10-29 15:40:54 +0900

    refactor: timetable선택시 색상 변경되도록

--- src/pages/participate/Participate_timetable/TimetableHooks/useTimetableSelection.ts
+++ src/pages/participate/Participate_timetable/TimetableHooks/useTimetableSelection.ts
@@ -1,11 +1,12 @@
 //handleSelect함수 따로 뺌ㅋ
 import { addMinutes, isAfter } from "date-fns";
-import { snap30,keyOf } from "@/utils/dateUtil";
+import { snap30, keyOf } from "@/utils/dateUtil";
 import type { ParticipateObject } from "@/apis/participate/participateTypes";
+import type React from "react";
 
-interface date{
-    start:Date;
-    end:Date;
+interface date {
+  start: Date;
+  end: Date;
 }
 export const useTimetableSelection = (
   selectedTimes: ParticipateObject[],
@@ -21,9 +22,7 @@ export const useTimetableSelection = (
     const eISO = e.toISOString();
     const key = keyOf(sISO, eISO);
 
-    const exists = selectedTimes.some(
-      (t) => keyOf(t.startAt, t.endAt) === key
-    );//중복 클릭이 존재하는지 체크하는 로직->선택된 시간 배열 안에 이미 선택한 시간이
+    const exists = selectedTimes.some((t) => keyOf(t.startAt, t.endAt) === key); //중복 클릭이 존재하는지 체크하는 로직->선택된 시간 배열 안에 이미 선택한 시간이
 
     setSelectedTimes((prev) =>
       exists
