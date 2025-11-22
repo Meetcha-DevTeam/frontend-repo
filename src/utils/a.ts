818e8d017119781b5641cc7e9d5aaf8491ffeb76
nayounsang <nayounsang722@gmail.com>
2025-11-22 16:43:49 +0900

    chore: delete temporary ts file to test

--- src/utils/a.ts
+++
@@ -1,9 +0,0 @@
-import { foo } from "./aa";
-
-const bar: number | null = null;
-
-console.log(foo?.includes("a"));
-console.log(foo.includes("a"));
-
-console.log(bar ? bar + 10 : 0);
-console.log(bar + 10);
--- src/utils/aa.ts
+++
@@ -1,5 +0,0 @@
-export const foo: string | null = null;
-
-const bar: string | null = null;
-
-console.log(bar.includes("a"));
