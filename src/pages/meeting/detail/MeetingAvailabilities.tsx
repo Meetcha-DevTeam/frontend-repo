00c78506f1849c80770de7e3bfc9cbb7ad1ffd70
nayounsang <nayounsang722@gmail.com>
2025-12-24 16:25:56 +0900

    refactor: 필요한 ui 데이터만 props로 받아 map 생성을 1번만 하도록 함

--- src/pages/meeting/detail/MeetingAvailabilities.tsx
+++ src/pages/meeting/detail/MeetingAvailabilities.tsx
@@ -32,41 +32,43 @@ export const MeetingAvailabilitiesContainer = ({ meetingId }: { meetingId: strin
         setLoading(false);
       });
   }, [meetingId]);
-  const initialCount = useMemo(() => {
-    if (!availabilities) return 1;
-    const participantsTime = splitParticipantsTime(availabilities.participants);
-    const maxCount = Math.max(...participantsTime.values(), 1);
-    return maxCount;
-  }, [availabilities]);
+
   // TODO: 각 케이스 디자인 시안 요구
   if (loading) return null;
   if (error) return null;
   if (!availabilities) return null;
   if (availabilities.count === 0) return null;
 
+  const participantsTime = splitParticipantsTime(availabilities.participants);
+  const initialCount = Math.max(...participantsTime.values(), 1);
+  const totalCount = availabilities.count;
+
   return (
-    <MeetingAvailabilitiesContent availabilities={availabilities} initialCount={initialCount} />
+    <MeetingAvailabilitiesContent
+      participantsTime={participantsTime}
+      initialCount={initialCount}
+      totalCount={totalCount}
+    />
   );
 };
 
 const MeetingAvailabilitiesContent = ({
-  availabilities,
+  participantsTime,
   initialCount,
+  totalCount,
 }: {
-  availabilities: MeetingAvailabilities;
+  /**Map<ISO format time(30분 단위), 가능한 참여자 수> */
+  participantsTime: Map<string, number>;
   initialCount: number;
+  totalCount: number;
 }) => {
   const [selectedCount, setSelectedCount] = useState<number>(initialCount);
-  const isUpperBound = selectedCount >= availabilities.count;
+  const isUpperBound = selectedCount >= totalCount;
   const isLowerBound = selectedCount <= 1;
 
   const calendarEvents = useMemo<EventInput[]>(() => {
-    const participantsTime = splitParticipantsTime(availabilities.participants);
-    console.log({ participantsTime });
     const filteredTime = filterParticipantsTime(participantsTime, selectedCount);
-    console.log({ filteredTime });
     const enabledTimes = mergeParticipantsTime(filteredTime);
-    console.log({ enabledTimes });
     return enabledTimes.map((time) => ({
       start: time.startAt,
       end: time.endAt,
@@ -75,7 +77,7 @@ const MeetingAvailabilitiesContent = ({
       borderColor: "#ff7842",
       classNames: [styles.calendarEvent],
     }));
-  }, [availabilities.participants, selectedCount]);
+  }, [participantsTime, selectedCount]);
 
   return (
     <div className={styles.container}>
@@ -90,11 +92,11 @@ const MeetingAvailabilitiesContent = ({
             name="count"
             className={styles.title__inputContainer__input}
             min={1}
-            max={availabilities.count}
+            max={totalCount}
             value={selectedCount}
             onChange={(e) => {
               const numValue = parseInt(e.target.value);
-              if (numValue > availabilities.count) {
+              if (numValue > totalCount) {
                 return;
               }
               setSelectedCount(numValue);
@@ -126,7 +128,7 @@ const MeetingAvailabilitiesContent = ({
               <BiChevronDown className={styles.title__inputContainer__iconContainer__icon} />
             </button>
           </div>
-          <p>/ {availabilities.count}</p>
+          <p>/ {totalCount}</p>
         </div>
       </div>
       <div className={styles.calendarContainer}>
