15aaceffa52590fd34433818bafae8f2ab8b6469
Chris Ahn <chrisahn1007@gmail.com>
2025-12-24 19:16:00 +0900

    refactor: LoginCompleteContainer에서 setTimeout으로 로그인 처리 완료를 기다리지 않고 api 응답을 기준으로 전개하도록 수정

--- src/pages/login_complete/components/LoginCompleteContainer.tsx
+++ src/pages/login_complete/components/LoginCompleteContainer.tsx
@@ -16,20 +16,18 @@ const LoginCompleteContainer = () => {
 
     (async () => {
       try {
-        await sendAuthCode(code, addr);
+        if (await sendAuthCode(code, addr)) {
+          const reserved = sessionStorage.getItem("reservedNavigate");
+          if (reserved) {
+            window.location.href = reserved;
+            return;
+          }
+          navigate("/schedule");
+        }
       } catch (e) {
         console.error(e);
       }
     })();
-
-    setTimeout(() => {
-      const reserved = sessionStorage.getItem("reservedNavigate");
-      if (reserved) {
-        window.location.href = reserved;
-        return;
-      }
-      navigate("/schedule");
-    }, 5000);
   }, [params]);
   return (
     <div className="login_complete_container">
