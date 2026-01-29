3f19ab6e4733ced93bfb19e90d3a83d0c8c4c0c5
Chris Ahn <chrisahn1007@gmail.com>
2026-01-29 17:31:20 +0900

    fix: Loader 수정

--- src/pages/schedule/monthly_schedule/MonthlyCalendarTile.tsx
+++ src/pages/schedule/monthly_schedule/MonthlyCalendarTile.tsx
@@ -9,12 +9,13 @@ export interface Event {
 
 const CalendarTile = ({ date, schedules }: { date: Date; schedules: UserScheduleData[] }) => {
   const eventNames: Event[] = [];
-
-  schedules.forEach((schedule) => {
-    if (dateFormatter(new Date(schedule.startAt)) === dateFormatter(date)) {
-      eventNames.push({ id: schedule.eventId, name: schedule.title });
-    }
-  });
+  if (schedules) {
+    schedules.forEach((schedule) => {
+      if (dateFormatter(new Date(schedule.startAt)) === dateFormatter(date)) {
+        eventNames.push({ id: schedule.eventId, name: schedule.title });
+      }
+    });
+  }
 
   return <EventTagBox eventNames={eventNames} />;
 };
