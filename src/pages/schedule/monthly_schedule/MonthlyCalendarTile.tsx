fb516d915fca2388049cd6b3e3fb1e75556194e7
Chris Ahn <chrisahn1007@gmail.com>
2026-01-29 17:17:50 +0900

    chore: 일부 코드 리팩토링

---
+++ src/pages/schedule/monthly_schedule/MonthlyCalendarTile.tsx
@@ -0,0 +1,22 @@
+import type { UserScheduleData } from "@/apis/participate/participateTypes";
+import { dateFormatter } from "@/utils/dateFormatter";
+import EventTagBox from "./EventTagBox";
+
+export interface Event {
+  id: string;
+  name: string;
+}
+
+const CalendarTile = ({ date, schedules }: { date: Date; schedules: UserScheduleData[] }) => {
+  const eventNames: Event[] = [];
+
+  schedules.forEach((schedule) => {
+    if (dateFormatter(new Date(schedule.startAt)) === dateFormatter(date)) {
+      eventNames.push({ id: schedule.eventId, name: schedule.title });
+    }
+  });
+
+  return <EventTagBox eventNames={eventNames} />;
+};
+
+export default CalendarTile;
--- src/pages/schedule/monthly_schedule/MonthlyScheduleView.tsx
+++ src/pages/schedule/monthly_schedule/MonthlyScheduleView.tsx
@@ -1,18 +1,12 @@
 import Calendar from "react-calendar";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "./Calendar.scss";
-import EventTagBox from "./EventTagBox";
-import { dateFormatter } from "@/utils/dateFormatter";
 import type { UserScheduleData } from "@/apis/participate/participateTypes";
 import { useMonthlySwiperController } from "@/hooks/useMonthlySwiperController";
+import CalendarTile from "./MonthlyCalendarTile";
 
 interface Props {
   schedules: UserScheduleData[];
-}
-
-export interface Event {
-  id: string;
-  name: string;
 }
 
 const MonthlyScheduleView = ({ schedules }: Props) => {
@@ -40,19 +34,7 @@ const MonthlyScheduleView = ({ schedules }: Props) => {
                 view="month"
                 onViewChange={() => {}}
                 showNeighboringMonth={false}
-                tileContent={({ date }) => {
-                  const eventNames: Event[] = [];
-                  if (schedules) {
-                    schedules.forEach((schedule) => {
-                      const date1 = dateFormatter(new Date(schedule.startAt));
-                      const date2 = dateFormatter(new Date(date));
-                      if (date1 === date2) {
-                        eventNames.push({ id: schedule.eventId, name: schedule.title });
-                      }
-                    });
-                  }
-                  return <EventTagBox eventNames={eventNames} />;
-                }}
+                tileContent={({ date }) => <CalendarTile date={date} schedules={schedules} />}
                 formatDay={(_, date) => date.toLocaleString("en", { day: "numeric" })}
                 formatShortWeekday={(_, date) => date.toLocaleString("en-US", { weekday: "short" })}
                 onActiveStartDateChange={() => {}}
